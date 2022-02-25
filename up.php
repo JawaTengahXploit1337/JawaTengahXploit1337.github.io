5b399b7f97e5c25cc71f5f2ee84962bc67777351
JawaTengahXploit⁨¹³³⁷⁩ <94507398+JawaTengahXploit1337@users.noreply.github.com>
2022-02-26 00:49:47 +0700

    Add files via upload

---
+++ up.php
@@ -0,0 +1,80 @@
+
+<!doctype html>
+<link rel="icon" type="image/png" href="https://d1fdloi71mui9q.cloudfront.net/fqympqV9TzyPp8wU9Z8B_4F6D0r1hvf58eWpI">
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <meta name="author" content="Copyright @2021"> <!-- Copyright @2021 -->
+<link rel="icon" type="image/png" href="https://d1fdloi71mui9q.cloudfront.net/fqympqV9TzyPp8wU9Z8B_4F6D0r1hvf58eWpI">
+    <meta name="description" content="Uploader Buat Mastah :)">
+    <meta name="theme-color" content="#000">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+<link href="https://fonts.googleapis.com/css?family=Girassol&display=swap" rel="stylesheet">
+    <link rel="icon" href="https://d1fdloi71mui9q.cloudfront.net/fqympqV9TzyPp8wU9Z8B_4F6D0r1hvf58eWpI" type="image/jpg">
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.1.0/css/font-awesome.min.css'>
+<link rel="icon" type="image/png" href="https://d1fdloi71mui9q.cloudfront.net/fqympqV9TzyPp8wU9Z8B_4F6D0r1hvf58eWpI">
+    <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script> 
+		<script src='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.2.0/js/bootstrap.min.js'></script>
+		<script src='https://unpkg.com/sweetalert/dist/sweetalert.min.js'></script>
+    <title>Uploader By ./W4D3R 1337</title>
+    <style>
+html,body,.container{
+            background: url(http://3.bp.blogspot.com/-GaLf17GTJSY/VBfS0rS4vAI/AAAAAAAAES8/eaW3b326Yus/s1600/monozcore_petir.gif) no-repeat center center/cover;
+            position: fixed;
+            margin: auto;
+            height: 100%;
+            top: 0; bottom: 0; left: 0; right:0;
+            font-family: 'Girassol';
+            color: #fff;
+        }
+    	#ico{width:200px;height:150px;}
+    </style>
+  </head>
+  <body>
+<iframe widllth="0%" height="0" scrolling="no" frameborder="no" loop="true" allow="autoplay" src="https://jawatengahxploit1337.github.io/LEMAH%20TELES%20-%20NIKISUKA%20ft%20LITA%20MANDA%20(%20Official%20Music%20Video%20UYE%20tone%20).mp3"></iframe>
+    <div class="container text-center mx-auto">
+    	<h2 class="mt-3"><font color="#00BFFF">Heker Kok Uploader<br></font></h2>
+<center> 
+   <img src="https://j.top4top.io/p_2243hnh1z0.png" height="250"> 
+<br>
+<br>
+<h4 class="mt-3"><font color="#00BFFF">Upload Script Html Lu</font></h4><br>
+    	<form method="post" enctype="multipart/form-data">
+    		<div class="row">
+    			<div class="col">
+    				<input id="previews" placeholder="Pilih File" class="form-control bg-transparent text-white" readonly="readonly">
+    			</div>
+    			
+    			<div class="col">
+    				<div class="input-group">
+    					<div class="custom-file">
+    						<input type="file" name="randsx" class="custom-file-input bg-transparent" id="FileSel">
+    						<label class="custom-file-label bg-transparent" for="previews"></label>
+    					</div>
+    				</div>
+    			</div>
+    		</div>
+    		<button type="submit" name="upload" class="btn btn-secondary btn-block bg-transparent mt-3" id="load" data-loading-text="<i class='fa fa-refresh fa-spin'></i> Sabar Tod !!"><font color="#00BFFF">Upload Cok!</font></button>
+    	</form><hr>
+
+<footer>
+	<body dir="ltr" alink="#00FFFF" background="transparan" bgcolor="#000000" link="grey" text="grey" vlink="white">Jawa Tengah Xploit # <font color="#00FFFF"><a href="#">All Team & Friends</font>
+</footer>
+    </div>
+		<script>
+			document.getElementById('FileSel').onchange = function()
+			{
+				document.getElementById('previews').value = this.value;
+			};
+			
+			$('.btn').on('click', function(){
+				var $this = $(this);
+				$this.button('loading');
+			});
+			
+		</script>
+  </body>
+</html>
